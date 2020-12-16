fb8ed052c333b25c7fc209c241e9f219ad099a12
dssorokin <d.sorokin@onvoya.com>
2020-12-16 21:49:08 +0300

    initial commit

---
+++ src/main/java/com/telegram/debt/DebtController.java
@@ -0,0 +1,7 @@
+package com.telegram.debt;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DebtController {
+}
---
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -0,0 +1,45 @@
+create table "group"
+(
+	group_id bigserial not null
+		constraint group_pkey
+			primary key,
+	group_name text
+)
+;
+
+create table users
+(
+	user_id bigserial not null
+		constraint users_pk
+			primary key,
+	name text,
+	shipment_date date,
+	group_id bigserial not null
+		constraint users_group_group_id_fk
+			references "group"
+				on delete cascade
+);
+
+
+
+create table debts
+(
+	debt_id bigserial not null
+		constraint table_name_pkey
+			primary key,
+	from_user_id bigserial not null
+		constraint table_name_users_user_id_fk
+			references users,
+	to_user_id bigserial not null
+		constraint table_name_users_user_id_fk_2
+			references users,
+	created_date date,
+	amount numeric(15,2)
+);
+
+
+
+
+
+
+
