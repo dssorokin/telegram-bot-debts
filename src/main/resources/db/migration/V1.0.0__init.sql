23421d68830412ac974988ea16db71dba5ca3c97
daniilSorokin94 <d.sorokin@onvoya.com>
2021-03-29 23:58:38 +0300

    create database schema

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,41 +1,42 @@
-create table "group"
+create table if not exists group_chat
 (
-	group_id bigserial not null
-		constraint group_pkey
-			primary key,
-	group_name text
+    group_id bigserial not null
+    constraint group_pkey
+    primary key,
+    group_name text
 )
 ;
 
-create table users
+create table if not exists users
 (
-	user_id bigserial not null
-		constraint users_pk
-			primary key,
-	name text,
-	shipment_date date,
-	group_id bigserial not null
-		constraint users_group_group_id_fk
-			references "group"
-				on delete cascade
+    user_id bigserial not null
+    constraint users_pk
+    primary key,
+    name text,
+    shipment_date date,
+    group_id bigserial not null
+    constraint users_group_group_id_fk
+    references "group_chat"
+    on delete cascade
 );
 
 
 
-create table debts
+create table if not exists debts
 (
-	debt_id bigserial not null
-		constraint table_name_pkey
-			primary key,
-	from_user_id bigserial not null
-		constraint table_name_users_user_id_fk
-			references users,
-	to_user_id bigserial not null
-		constraint table_name_users_user_id_fk_2
-			references users,
-	created_date date,
-	amount numeric(15,2)
-);
+    debt_id bigserial not null
+    constraint table_name_pkey
+    primary key,
+    description text not null,
+    from_user_id bigserial not null
+    constraint table_name_users_user_id_fk
+    references users,
+    to_user_id bigserial not null
+    constraint table_name_users_user_id_fk_2
+    references users,
+    created_date date,
+    amount numeric(15,2)
+    );
 
 
 
