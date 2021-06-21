d60fda0d3a0f51eb4e59b2ec58e3b8233547acf2
daniilSorokin94 <d.sorokin@onvoya.com>
2021-06-21 20:05:21 +0300

    create objectMapper as bean

--- src/main/java/com/telegram/debt/DebtAccountManagerImpl.java
+++ src/main/java/com/telegram/debt/DebtAccountManagerImpl.java
@@ -1,16 +1,13 @@
 package com.telegram.debt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.telegram.debt.model.Debt;
 import com.telegram.debt.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,12 +24,16 @@ public class DebtAccountManagerImpl implements DebtAccountManager {
 	@Autowired
 	private DebtDao debtDao;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Override
 	public void addDebtForGroup(BigDecimal debtAmount, Long lenderId, Long borrowerId,
 								Long groupId, String debtDescription) throws NoSuchUserException {
 		User lender = userDao.findById(lenderId).orElseThrow(() -> new NoSuchUserException(lenderId));
 		User borrower = userDao.findById(borrowerId).orElseThrow(() -> new NoSuchUserException(borrowerId));
 
+		updateUsersDebtsSummary(lender, borrower, debtAmount);
 
 
 		Debt newDebt = new Debt();
@@ -42,8 +43,7 @@ public class DebtAccountManagerImpl implements DebtAccountManager {
 		debtDao.save(newDebt);
 	}
 
-	private void updateUsersDebtsSummary(User lender, User borrowerId, BigDecimal debtAmount) {
-
+	private void updateUsersDebtsSummary(User lender, User borrower, BigDecimal debtAmount) {
 	}
 
 	@Override
--- src/main/java/com/telegram/debt/DebtBotConfiguration.java
+++ src/main/java/com/telegram/debt/DebtBotConfiguration.java
@@ -1,8 +1,14 @@
 package com.telegram.debt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DebtBotConfiguration {
 
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }
