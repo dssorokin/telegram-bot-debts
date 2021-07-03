7a872d17d357420d3a16c73f88f7429e4c269836
dssorokin <d.sorokin@onvoya.com>
2021-07-03 14:47:11 +0300

    removed redundant modificator

--- src/main/java/com/telegram/debt/UserManager.java
+++ src/main/java/com/telegram/debt/UserManager.java
@@ -3,7 +3,7 @@ package com.telegram.debt;
 import com.telegram.debt.model.User;
 
 public interface UserManager {
-	public void registerUserInGroup(Long user, Long groupId, String userName);
+	void registerUserInGroup(Long user, Long groupId, String userName);
 
-	public User findUserByLink(String userLink);
+	User findUserByLink(String userLink);
 }
